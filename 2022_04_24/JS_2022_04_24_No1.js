8eecb031a3c1857912dd4ffcd5c0375bf7c78ecb
HoCG <rhwkd5012@naver.com>
2022-04-25 15:31:14 +0900

    상속 실험

--- 2022_04_24/JS_2022_04_24_No1.js
+++ 2022_04_24/JS_2022_04_24_No1.js
@@ -5,5 +5,13 @@ class Person {
   }
 }
 
+class Police extends Person {
+  callName(){
+    return this.name + " 경찰"
+  }
+}
+
 const junho = new Person('준호', 26);
+const minsu = new Police('민수', 23)
 console.log(junho.name);
+console.log(minsu.callName());
